7a73b47b41cccdf94c8e0389191d80f032a5f2d5
felpsGSantos <felipe.gomes04@hotmail.com>
2023-06-14 11:29:10 -0300

    cadastro de colaborador

---
+++ conectar_db.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhostname = '10.23.45.24';
+$dbusuario = 'root';
+$dbsenha='';
+$dbname='dbestacionamento';
+
+$conexao = mysqli_connect ($dbhostname, $dbusuario, $dbsenha, $dbname);
+
+?>
